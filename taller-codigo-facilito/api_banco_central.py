9490446caa8523e3f2510a3440ae0a90c22eb9ae
emi <emazzurque@gmail.com>
2020-05-11 00:11:03 -0300

    api b c

---
+++ taller-codigo-facilito/api_banco_central.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+import requests 
+import json
+
+
+
+token ='eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE2MjA2MjYzMzMsInR5cGUiOiJleHRlcm5hbCIsInVzZXIiOiJlbWF6enVycXVlQGdtYWlsLmNvbSJ9.7OcIFK4TivgRE8fjErZo9I8VEjq4FEQUiXiJKv4mAY7zUuImaDkNXWgPakdgVsSRentKhDzySSUdtQCaWDlucA'
+url = "https://api.estadisticasbcra.com/m2_privado_variacion_mensual"
+
+
+if __name__ == "__main__":
+    
+      
+    headers = {'Authorization': 'Bearer ' + token}
+
+    res = requests.get( url, headers=headers)
+
+    if res.ok:       
+        print(res.url)
+        response_json = res.json() #Dic
+        for x in response_json:
+            print(x)
