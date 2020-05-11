61a03b7f7df085fb6738e9b6b2d0eaa22a33eddb
emi <emazzurque@gmail.com>
2020-05-11 01:06:34 -0300

    refactoring

--- taller-codigo-facilito/api_banco_central.py
+++ taller-codigo-facilito/api_banco_central.py
@@ -6,7 +6,8 @@ import json
 
 token ='eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE2MjA2MjYzMzMsInR5cGUiOiJleHRlcm5hbCIsInVzZXIiOiJlbWF6enVycXVlQGdtYWlsLmNvbSJ9.7OcIFK4TivgRE8fjErZo9I8VEjq4FEQUiXiJKv4mAY7zUuImaDkNXWgPakdgVsSRentKhDzySSUdtQCaWDlucA'
 url = "https://api.estadisticasbcra.com/m2_privado_variacion_mensual"
-
+lista=[]
+listaordenada=[]
 
 if __name__ == "__main__":
     
@@ -19,4 +20,18 @@ if __name__ == "__main__":
         print(res.url)
         response_json = res.json() #Dic
         for x in response_json:
-            print(x)
+            valor=x['v']
+            
+            
+            
+            lista.append(valor)
+
+            
+            #print(x['v'])
+            #print(x['d'])
+            #print("************************")
+        lista.sort()
+        print(lista[0])
+        print('mas barato')
+      
+        
